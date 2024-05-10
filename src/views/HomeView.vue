a49954fc764e645c8a37d18e036240bef71af0dc
Florian Bender <fb+git@quantumedia.de>
2024-05-10 21:46:43 +0200

    Add first attempt at simple "EventCard" component
    This will be refactored immediately in the next commit, but it's a start.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
+import EventCard from '../components/EventCard.vue' //TODO: replace '..' with '@' to make it an absolute path
 </script>
 
 <template>
   <div class="home">
+    <EventCard />
   </div>
 </template>
