e77eb0a678d02c63455dc54a8be620588429eb08
Florian Bender <fb+git@quantumedia.de>
2024-05-11 20:06:13 +0200

    Add simple programmatic navigation
    A "register" button callback redirects you back to the event details.

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,21 @@
 <script setup>
+import { useRouter } from "vue-router";
 import { defineProps } from "vue";
 
-defineProps(["event"]);
+const props = defineProps(["event"]);
+const router = useRouter();
+
+const register = () => {
+  // Call to API
+  // If registered then redirect to event details
+  router.push({ // use router.replace() if you want to overwrite the last browser history entry
+    name: "EventDetails",
+    id: props.event.id, // explicitly passing ID is not required as we're naviging within the same "parent" route
+  });
+};
 </script>
 
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
