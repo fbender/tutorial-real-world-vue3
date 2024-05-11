c532f6a5626f54419769d26c13879ddb3a7af4a7
Florian Bender <fb+git@quantumedia.de>
2024-05-11 18:02:36 +0200

    Refactor to use nested routes for event pages
    Remove all the code duplication by introducing a "parent" layout for the event detail/register/edit views and introduce child routes.

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,31 +1,7 @@
 <script setup>
-import EventService from "@/services/EventService.js";
-import { onMounted, ref, computed } from "vue";
-
-const props = defineProps(["id"]);
-
-const event = ref(null);
-const id = computed(() => props.id);
-onMounted(() => {
-  EventService.getEvent(id.value)
-    .then((response) => {
-      event.value = response.data;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+const props = defineProps(["event"]);
 </script>
 
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <router-link :to="{ name: 'EventDetails', params: { id } }">Details</router-link> |
-      <router-link :to="{ name: 'EventRegister', params: { id } }">Register</router-link> |
-      <router-link :to="{ name: 'EventEdit', params: { id } }">Edit</router-link>
-    </div>
-    <p>Edit event here</p>
-  </div>
-  <strong v-else>Nothing (yet).</strong>
+  <p>Edit the event here</p>
 </template>
